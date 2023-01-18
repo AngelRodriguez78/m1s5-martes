793d416e5b976c21e50cd2c3a2cd1e5046f35adc
Angel Daniel RM <119967462+AngelRodriguez78@users.noreply.github.com>
2023-01-17 22:33:33 -0500

    Ejemplo de clase finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 //console.log("Que pedo2");
 //console.log("Que pedo3");
 
-var nombre = "juan";
+var nombre = "juan";  // se puede reasignat pero tiene otro alcance mas global.. no se recomienda en la actualizad
 var edad = 30;
 
 const fechaNacimiento ="17/08/1993"; // no se puede reasginar (no deberia)
@@ -14,6 +14,32 @@ console.log(apellido);
 apellido ="Gomez";
 console.log(apellido);
 console.log(edad);
+
+/*
+Esto es un comentario
+*/
+
+let nombreCompleto = "Jesus Cardenas"; // string(texto)
+let telefono = 123456789; // number (numero)
+let humano = true // Boolean (True/False) Indica que puede ser verdade o falso
+let direccion;
+let codigoPostal = null;
+/*
+console.log("nombreCompleto",typeof nombreCompleto);
+console.log("telefono", typeof telefono);
+console.log("humano", typeof humano);
+console.log("direccion", typeof direccion);
+console.log("codigoPostal", typeof codigoPostal);
+*/
+
+nombreCompleto= String(7);
+telefono = Number("987654321");
+
+console.log("nombreCompleto", nombreCompleto);
+console.log("telefono",  telefono);
+console.log("humano",  humano);
+console.log("direccion",  direccion);
+console.log("codigoPostal", codigoPostal);
 
 
 
---
+++ assets/js/main2.js
@@ -0,0 +1,26 @@
+"use strict";
+
+console.log("Console log desde otro.js");
+
+
+let carName ="volvo";
+let driver ="juan"
+let X=50;
+let a=5  // let a="5"
+let b=10 // Let b="10"
+
+let suma = a+b; // let suma = number(a) + number(b)
+let concatenacion = carName + driver;
+
+console.log("la suma es:", suma);
+console.log("La concatenacion es:", concatenacion);
+
+
+
+
+
+
+
+
+
+
