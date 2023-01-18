fe461829775c32aece5574847696f82d9e7279ef
Angel Daniel RM <119967462+AngelRodriguez78@users.noreply.github.com>
2023-01-17 21:39:00 -0500

    Entrando a tipo de datps

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
-console.log("Que pedo");
+"use strict";
+
+//console.log("Que pedo");
+//console.log("Que pedo2");
+//console.log("Que pedo3");
+
+var nombre = "juan";
+var edad = 30;
+
+const fechaNacimiento ="17/08/1993"; // no se puede reasginar (no deberia)
+
+let apellido ="Perez";
+console.log(apellido);
+apellido ="Gomez";
+console.log(apellido);
+console.log(edad);
+
+
+
+
+
+
+
