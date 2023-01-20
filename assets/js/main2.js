3689203f8d957565e28d00948128ea37d7159679
Angel Daniel RM <119967462+AngelRodriguez78@users.noreply.github.com>
2023-01-19 21:10:34 -0500

    operadores
    se agrego operadores js

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -8,6 +8,9 @@ let driver ="juan"
 let X=50;
 let a=5  // let a="5"
 let b=10 // Let b="10"
+let texto1 ="El conductor"
+let texto2 ="tiene un"
+
 
 let suma = a+b; // let suma = number(a) + number(b)
 let concatenacion = carName + driver;
@@ -15,8 +18,17 @@ let concatenacion = carName + driver;
 console.log("la suma es:", suma);
 console.log("La concatenacion es:", concatenacion);
 
+let concatenacion2 = (carName + " " +driver)
 
+console.log ("La concatenacion opcion 2:", concatenacion2);
 
+let concatenacion3 = (texto1 + " " + driver + " " + texto2 + " " + carName)
+
+console.log ("La concatenacion opcion 3:", concatenacion3);
+
+//string template en javascript
+let optimizacionConcatenacion = `El conductor ${driver} tiene un ${carName}`;
+console.log ("La concatenacion opcion 4:",optimizacionConcatenacion);
 
 
 
---
+++ assets/js/operadores.js
@@ -0,0 +1,29 @@
+console.log("Esto es el archivo de operadores")
+console.log(optimizacionConcatenacion);
+
+
+let numero1 = 10;
+let numero2 = 25;
+
+console.log(`la suma: ${numero1 + numero2}`);
+
+console.log(`la resta: ${numero2 - numero1}`);
+
+console.log(`la multiplicacion: ${numero1 * numero2}`);
+
+console.log(`el residuo de division: ${numero1 % numero2}`);
+
+// alert("Esto es un alert");
+
+let edad1 = 20;
+
+/*
+Quiero mostrar un mensaje si la persona es mayor de edad y  otro si la persona
+no es mayor de edad.
+*/
+
+if (edad1 > 17) {
+    console.log("Es mayor de edad");
+}
+else { console.log("Es menor de edad"); }
+
